f5acc56b96289ff1f2c64c8f858c4e4c6c287bde
cameron custer <custer.cameron@gmail.com>
2024-02-14 09:03:10 -0500

    impl birthday party

--- birthday_party/src/main.rs
+++ birthday_party/src/main.rs
@@ -1,3 +1,50 @@
+use rand::Rng;
+use std::sync::{Arc, Mutex};
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    const N: usize = 10;
+
+    struct Labyrinth {
+        have_eaten: Vec<bool>,
+        num_guests_who_ate: usize,
+        is_cupcake_available: bool,
+    }
+
+    let labyrinth = Arc::new(Mutex::new(Labyrinth {
+        have_eaten: vec![false; N],
+        num_guests_who_ate: 0,
+        is_cupcake_available: true,
+    }));
+
+    let guest_eating = Arc::new(Mutex::new(N));
+
+    for guest in 0..N {
+        let me = guest;
+        let labyrinth = labyrinth.clone();
+        let guest_eating = guest_eating.clone();
+        thread::spawn(move || loop {
+            if *guest_eating.lock().unwrap() == me {
+                let mut labyrinth = labyrinth.lock().unwrap();
+                if !labyrinth.have_eaten[me] {
+                    if !labyrinth.is_cupcake_available {
+                        // guest requests a cupcake
+                        labyrinth.is_cupcake_available = true;
+                    }
+                    labyrinth.have_eaten[me] = true;
+                    labyrinth.num_guests_who_ate += 1;
+                    labyrinth.is_cupcake_available = false;
+                    println!("Guest {} has eaten", me);
+                }
+            }
+        });
+    }
+
+    let mut rng = rand::thread_rng();
+    while labyrinth.lock().unwrap().num_guests_who_ate < N {
+        let guest = rng.gen_range(0..N);
+        *guest_eating.lock().unwrap() = guest;
+    }
+
+    println!("All guests have visited the labyrinth!");
 }
